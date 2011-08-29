d01cd5c97d7733fa30fe934f58d935658e1b1a63
thallesr <thallesr@gmail.com>
2011-08-29 12:11:19 +0000

    commit inicial

---
+++ trabalho1/dojo1.pro
@@ -0,0 +1,16 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-08-22T10:48:45
+#
+#-------------------------------------------------
+
+TARGET = dojo1
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ trabalho1/mainwindow.cpp
@@ -0,0 +1,50 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include "math.h"
+#include <QPixmap>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pBtn01,SIGNAL(clicked()),this,SLOT(conectar()));
+    connect(ui->BotaoFecha,SIGNAL(triggered()),this,SLOT(close()));
+   connect(ui->actionRand,SIGNAL(triggered()),this,SLOT(genRand()));
+   connect(ui->pBtn01,SIGNAL(clicked()),this,SLOT(exibeImagem()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+
+}
+void MainWindow::conectar(){
+    ui->label->setText("foi");
+}
+
+void MainWindow::genRand(){
+    srand(time(NULL));
+    qDebug() << "Sorteio: " << rand();
+}
+
+void MainWindow::exibeImagem(){
+   qDebug()<<" carrega imagem ";
+   QString local = "/home/pericolo/teste.png";
+   QPixmap *imagem = new QPixmap(local);
+   ui->imagem->setPixmap(*imagem);
+}
+
