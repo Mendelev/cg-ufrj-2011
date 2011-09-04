a616eae4dafcd44607b483e87ebf5887bf5d1f3c
thallesr <thallesr@0aa4eaa0-cd31-810a-043e-0c7173d71561>
2011-09-04 23:29:45 +0000

    talvez funcione o cropper

--- trabalho2/imagecropper.h
+++ trabalho2/imagecropper.h
@@ -1,10 +1,31 @@
 #ifndef IMAGECROPPER_H
 #define IMAGECROPPER_H
+#include <QPoint>
 
 class imageCropper
 {
+private:
+    uchar * newData;
+    uchar * vetor;
+    int oldHeight, oldWidth;
+    int newHeight ,newWidth;
+    double scaleAToBHeight, scaleAToBWidth;
+    QImage newImage;
+    QImage oldImage;
+    QPoint pontoInicial;
+
 public:
-    imageCropper();
+   void transform();
+
+   ImageResizer(QImage image, int newWidth, int newHeight);
+   ImageResizer();
+   int acessaPosicao(int x, int y);
+    int acessaPosicaoNew(int x, int y);
+   void aplicaCor(Cor cor,int i,int j);
+
+   Cor funcaoDeEscolha(int x,int y);
+
+
 };
 
 #endif // IMAGECROPPER_H
