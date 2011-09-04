fbb535e53cb688b35f098a6b4da8c59403efa19f
thallesr <thallesr@0aa4eaa0-cd31-810a-043e-0c7173d71561>
2011-09-04 20:45:47 +0000

    "Acho que funciona o metodo babaca"

--- trabalho1/imageresizer.h
+++ trabalho1/imageresizer.h
@@ -2,11 +2,13 @@
 #define IMAGERESIZER_H
 #include <QRgb>
 #include <QImage>
+#include <cor.h>
 
 class ImageResizer
 {
 private:
-
+    uchar * newData;
+    uchar * vetor;
     int oldHeight, oldWidth;
     int newHeight ,newWidth;
     float scaleAToBHeight, scaleAToBWidth;
@@ -18,6 +20,11 @@ public:
 
    ImageResizer(QImage image, int newWidth, int newHeight);
    ImageResizer();
+   int acessaPosicao(int x, int y);
+   void aplicaCor(Cor cor,int i,int j);
+
+   Cor funcaoDeEscolha(int x,int y);
+
 
 
 };
